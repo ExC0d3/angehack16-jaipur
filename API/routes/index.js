efa5828a8055ef37b29d72c4be21cd6adeb09563
ExC0d3 <sharma.abhinav503@gmail.com>
2016-07-10 12:03:09 +0530

    added bar graph plot end point

--- API/routes/index.js
+++ API/routes/index.js
@@ -2,9 +2,13 @@
 
 import {mailgun, mailDetails} from '../services/email';
 import {client} from '../services/sms';
+import {plotly} from '../services/plot';
+
+
+var fs = require('fs'); 
 var express = require('express');
 var router = express.Router();
-
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -51,4 +55,62 @@ router.get('/promote/sms',(req,res,next) => {
 	});
 });
 
+router.get('/user',(req,res,next)=>{
+	next();
+},(req,res,next)=>{
+	var file = path.resolve('data dumps','campaign_data.json');
+
+	fs.readFile('/home/abhinav/Hackathons/angehack16-jaipur/data dumps/campaign_data.json','utf-8',(err,data) =>  {
+		if(err)
+			res.send(err);
+		else
+			{
+					req['campaign_data'] = data;
+					next();
+			}	
+	});
+},(req,res,next) => {
+	fs.readFile('/home/abhinav/Hackathons/angehack16-jaipur/data dumps/user_data.json','utf-8',(err,data) => {
+		if(err)
+			res.send(err);
+		else
+		{
+			let campaign_data = JSON.parse(req.campaign_data);
+			let user_data = JSON.parse(data);
+			let userId = req.query.id;
+
+			let campaigns = user_data[userId]["my_campaigns"];
+			let plot_data = [];
+			campaigns.forEach((campaign) => {
+				campaign_data.forEach((cd) => {
+					if(cd["_id"] == campaign["c_id"]){
+						plot_data.push(cd);
+					}
+				});
+			});
+
+			let x = plot_data.map(datum => datum.name);
+			let y = plot_data.map(datum => datum.donation_received);
+
+			var graphOptions = {filename: "basic-bar", fileopt:"overwrite"};
+			/*plotly.plot([{x,y,type:"bar"}],graphOptions,(err,msg) => {
+				if(err)
+					res.send(err);
+				else
+					res.send(msg);
+			});*/
+
+			plotly.getImage({'data':[{x,y,type:"bar"}]},{format:'png',width:1000,height:500},(err,imgStream) => {
+				if(err)
+					res.send(err);
+				else
+					imgStream.pipe(fs.createWriteStream('img.png'));
+					res.sendFile(path.resolve('img.png'));
+			});
+		}
+			
+	});
+});
+
 module.exports = router;
+
---
+++ API/services/plot.js
@@ -0,0 +1,3 @@
+'use strict';
+
+export const plotly = require('plotly')('ExC0d3','s3t8yk37pq');
